8911a61ff802be0036c4c4dde95848353f4357d9
NidhitheCoder <nidhpv@gmail.com>
2021-03-28 21:53:57 +0530

    Add new user page created and updated

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,9 +1,11 @@
-import {Input,Flex,Text} from '@chakra-ui/react';
+import { Input, Flex, Text } from "@chakra-ui/react";
 
-function InputSection({label}:any) {
+function InputSection({ label }: any) {
   return (
     <Flex padding="10" justifyContent="flex-start" alignItems="center">
-      <Text whiteSpace="nowrap" mr="10" minW="15" fontWeight="bold">{label} </Text>
+      <Text whiteSpace="nowrap" mr="10" minW="15" fontWeight="bold">
+        {label}{" "}
+      </Text>
       <Input />
     </Flex>
   );
--- src/pages/AddUser.tsx
+++ src/pages/AddUser.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
+import InputSection from "../components/InputSection";
+import CustomButton from "../components/CustomButton";
 
 const AddUser = () => {
   return (
-    <Flex minH="100vh" justifyContent="center" alignItems="center" >
-      <Text color="green.500">Profile page</Text>
+    <Flex
+      minH="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Text color="green.500">Add new user page</Text>
+      <InputSection label="Name :" />
+      <InputSection label="Id :" />
+      <CustomButton
+        caption="Add User"
+        eventFun={() => console.log("Add new User")}
+      />
     </Flex>
   );
 };
