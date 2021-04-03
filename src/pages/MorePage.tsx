e4bb57bd3b5f999e5b42416985ebd7d9506dc074
NidhitheCoder <nidhpv@gmail.com>
2021-04-03 13:31:55 +0530

    Social media aacount lists added

--- src/pages/MorePage.tsx
+++ src/pages/MorePage.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
+import CustomButton from "../components/CustomButton";
 
 const More = () => {
+  const socialList = [
+    { name: "facebook", bgColor: "blue.500" },
+    { name: "instagram", bgColor: "orange.400" },
+    { name: "twitter", bgColor: "blue.300" },
+    { name: "mail", bgColor: "red.600" },
+  ];
   return (
-    <Flex minH="90vh" justifyContent="center" alignItems="center" >
-      <Text color="green.500">More page</Text>
+    <Flex
+      minH="90vh"
+      justifyContent="center"
+      flexDirection="column"
+      gridGap="5"
+      alignItems="center"
+    >
+      <Text
+        color="green.500"
+        fontSize="24"
+        fontWeight="bold"
+        textColor="blue.400"
+      >
+        Connect us with:
+      </Text>
+      {socialList.map((item) => (
+        <CustomButton
+          caption={item.name}
+          color="white"
+          bgColor={item.bgColor}
+          radious="5"
+          width="56"
+          hovColor="green"
+        />
+      ))}
     </Flex>
   );
 };
